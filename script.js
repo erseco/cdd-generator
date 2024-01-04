badfed6717ef21591bac3e01a65f57d153581aac
erseco <erseco@users.noreply.github.com>
2024-01-04 10:40:59 +0000

    deploy: 8e4bbbcc8d7c35fa7d0287e1a907781691c02f61

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Funcion para quitar las tildes para uarlo en la búsqueda de texto
 function removeDiacritics(str) {
   return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
@@ -29,8 +28,14 @@ function createCheckbox(area, competencia, etapa, nivel, indicador) {
   checkbox.setAttribute('data-nivel', nivel.nivel);
   checkbox.setAttribute('data-indicador', indicador.indicador);
   checkbox.setAttribute('data-indicador-titulo', indicador.titulo);
-  checkbox.setAttribute('name', `indicador-${indicador.indicador}-comp-${competencia.competencia}`);
-  checkbox.setAttribute('id', `indicador-${indicador.indicador}-comp-${competencia.competencia}`);
+  checkbox.setAttribute(
+    'name',
+    `indicador-${indicador.indicador}-comp-${competencia.competencia}`
+  );
+  checkbox.setAttribute(
+    'id',
+    `indicador-${indicador.indicador}-comp-${competencia.competencia}`
+  );
 
   return checkbox;
 }
@@ -44,21 +49,22 @@ function createLabel(indicador) {
 
 function filterLevel(nivel) {
   const levelChecks = {
-    "A1": 'check-a1',
-    "A2": 'check-a2',
-    "B1": 'check-b1',
-    "B2": 'check-b2',
-    "C1": 'check-c1',
-    "C2": 'check-c2',
+    A1: 'check-a1',
+    A2: 'check-a2',
+    B1: 'check-b1',
+    B2: 'check-b2',
+    C1: 'check-c1',
+    C2: 'check-c2',
   };
   const levelCheck = levelChecks[nivel.nivel];
   return levelCheck && !document.getElementById(levelCheck).checked;
 }
 
-
 function handleTextSearch(area, competencia, etapa, nivel, indicador) {
-  const text = removeDiacritics(document.getElementById('search').value.toLowerCase());
-  if (text === "") return false;
+  const text = removeDiacritics(
+    document.getElementById('search').value.toLowerCase()
+  );
+  if (text === '') return false;
   const texts = [
     area.titulo,
     competencia.titulo,
@@ -68,33 +74,60 @@ function handleTextSearch(area, competencia, etapa, nivel, indicador) {
     nivel.titulo,
     indicador.titulo,
     nivel.afirmaciones_desempeño,
-    nivel.ejemplos.join(","),
-    ].join(' ').toLowerCase();
+    nivel.ejemplos.join(','),
+  ]
+    .join(' ')
+    .toLowerCase();
 
-    // Busca la cadena de texto en los textos combinados
+  // Busca la cadena de texto en los textos combinados
   return !removeDiacritics(texts).includes(text);
 }
 
 function createRow(area, competencia, etapa, nivel, indicador, lastCells) {
-  const row = document.createElement("tr");
+  const row = document.createElement('tr');
 
-    // Crea celdas
-  const areaCell = createCell("td", `<strong>${area.area}.</strong> ${area.titulo}`, "area");
-  const competenciaCell = createCell("td", `<strong>${competencia.competencia}.</strong> ${competencia.titulo}`, "competencia");
-  const etapaCell = createCell("td", `<strong>${etapa.etapa}.</strong> ${etapa.titulo}`, "etapa");
-  const nivelCell = createCell("td", `<strong>${nivel.nivel}.</strong> ${nivel.titulo}`, "nivel");
-  const indicadorCell = document.createElement("td");
-  const desempenoCell = createCell("td", nivel.afirmaciones_desempeño + "<br /><strong>Ejemplos:</strong><br /><ul><li>" + nivel.ejemplos.join('</li><li>') + "</li></ul>", "desempeno");
+  // Crea celdas
+  const areaCell = createCell(
+    'td',
+    `<strong>${area.area}.</strong> ${area.titulo}`,
+    'area'
+  );
+  const competenciaCell = createCell(
+    'td',
+    `<strong>${competencia.competencia}.</strong> ${competencia.titulo}`,
+    'competencia'
+  );
+  const etapaCell = createCell(
+    'td',
+    `<strong>${etapa.etapa}.</strong> ${etapa.titulo}`,
+    'etapa'
+  );
+  const nivelCell = createCell(
+    'td',
+    `<strong>${nivel.nivel}.</strong> ${nivel.titulo}`,
+    'nivel'
+  );
+  const indicadorCell = document.createElement('td');
+  const desempenoCell = createCell(
+    'td',
+    nivel.afirmaciones_desempeño +
+      '<br /><strong>Ejemplos:</strong><br /><ul><li>' +
+      nivel.ejemplos.join('</li><li>') +
+      '</li></ul>',
+    'desempeno'
+  );
 
-    // Añade clases
+  // Añade clases
   etapaCell.classList.add('d-none', 'd-md-table-cell');
   desempenoCell.classList.add('d-none', 'd-md-table-cell');
 
-    // Agregamos el color de fondo a la fila dependiendo del área
+  // Agregamos el color de fondo a la fila dependiendo del área
   row.classList.add(`area${area.area}`);
 
-    // Agrega tooltips
-  [areaCell, competenciaCell, etapaCell, nivelCell].forEach(cell => addTooltip(cell));
+  // Agrega tooltips
+  [areaCell, competenciaCell, etapaCell, nivelCell].forEach(cell =>
+    addTooltip(cell)
+  );
 
   // Construye la celda del indicador
   const label = createLabel(indicador);
@@ -103,69 +136,73 @@ function createRow(area, competencia, etapa, nivel, indicador, lastCells) {
   indicadorCell.append(label);
 
   // Comprueba y fusiona celdas si el contenido es el mismo que el de la fila anterior
-  [areaCell, competenciaCell, etapaCell, nivelCell, desempenoCell].forEach((cell, index) => {
-    const lastCell = lastCells[index];
+  [areaCell, competenciaCell, etapaCell, nivelCell, desempenoCell].forEach(
+    (cell, index) => {
+      const lastCell = lastCells[index];
 
-    // Agregamos la celda del indicador justo antes del índice 4 (desmpenoCell)
-    if (index == 4)
-      row.appendChild(indicadorCell);
+      // Agregamos la celda del indicador justo antes del índice 4 (desmpenoCell)
+      if (index === 4) {
+        row.appendChild(indicadorCell);
+      }
 
-    if (lastCell && lastCell.textContent === cell.textContent) {
-      lastCell.rowSpan++;
-      cell.style.display = "none";
-    } else {
-      row.appendChild(cell);
-            lastCells[index] = cell;  // Actualiza lastCells para la siguiente iteración
-          }
-        });
+      if (lastCell && lastCell.textContent === cell.textContent) {
+        lastCell.rowSpan++;
+        cell.style.display = 'none';
+      } else {
+        row.appendChild(cell);
+        lastCells[index] = cell; // Actualiza lastCells para la siguiente iteración
+      }
+    }
+  );
 
   return row;
 }
 
-
-
-var data;
-var tableBody;
+let data;
+let tableBody;
 
 function fillTable(data) {
-    // Limpiar las filas existentes, excepto la primera
+  // Limpiar las filas existentes, excepto la primera
   $('#mrcdd-table tr').not(':first').remove();
 
-    // Obtener el cuerpo de la tabla
-  const tableBody = document.querySelector("#table-body");
+  // Obtener el cuerpo de la tabla
+  const tableBody = document.querySelector('#table-body');
 
-    // Inicializar las últimas celdas para la gestión de rowspan
-  let lastCells = [];
+  // Inicializar las últimas celdas para la gestión de rowspan
+  const lastCells = [];
 
-    // Iterar sobre los datos y crear las filas
+  // Iterar sobre los datos y crear las filas
   for (const area of data) {
     for (const competencia of area.competencias) {
       for (const etapa of competencia.etapas) {
         for (const nivel of etapa.niveles) {
-
           // Filtrar por nivel
           if (filterLevel(nivel)) continue;
 
           for (const indicador of nivel.indicadores_logro) {
-
             // Manejar la búsqueda de texto
-            if (handleTextSearch(area, competencia, etapa, nivel, indicador)) continue;
+            if (handleTextSearch(area, competencia, etapa, nivel, indicador)) {
+              continue;
+            }
 
             // Crear una nueva fila
-            const row = createRow(area, competencia, etapa, nivel, indicador, lastCells);
+            const row = createRow(
+              area,
+              competencia,
+              etapa,
+              nivel,
+              indicador,
+              lastCells
+            );
 
             // Añadir la fila al cuerpo de la tabla
             tableBody.appendChild(row);
-            
           }
-
         }
       }
     }
   }
-
 }
-
 
 function loadData(url, callback) {
   fetch(url)
@@ -181,163 +218,183 @@ function loadData(url, callback) {
       callback(data);
     })
     .catch(error => {
-      console.error('There has been a problem with your fetch operation:', error);
+      console.error(
+        'There has been a problem with your fetch operation:',
+        error
+      );
     });
 }
 
-
 // Cargando datos del yml:
-loadData("mrcdd.yml?v=1699190906", fillTable);
-
+loadData('mrcdd.yml?v=1704364859', fillTable);
 
 // Mostrar el modal
 const generarBtn = document.querySelector('.btn-generar');
 generarBtn.addEventListener('click', () => {
-	generarTexto();
+  generarTexto();
 });
+
+
+const resetBtn = document.querySelector('.btn-reset');
+resetBtn.addEventListener('click', () => {
+
+  // Código JavaScript para desmarcar todos los checkbox con nombres de clase específicos
+  const checkboxes = document.querySelectorAll('.form-check-input.indicator-checkbox');
+  checkboxes.forEach(checkbox => {
+      checkbox.checked = false;
+  });
+
+});
+
+
+
+
 
 // Obtener los checkboxes marcados y generar el texto para el modal
 function generarTexto() {
-
-
   // Crear un objeto que contenga las áreas, competencias e indicadores seleccionados
   const selectedData = {};
 
   // Recorrer los checkboxes seleccionados
-  const checkedCheckboxes = document.querySelectorAll('input.indicator-checkbox[type="checkbox"]:checked');
+  const checkedCheckboxes = document.querySelectorAll(
+    'input.indicator-checkbox[type="checkbox"]:checked'
+  );
   checkedCheckboxes.forEach(checkbox => {
-
-    const indicador = checkbox.getAttribute("data-indicador");
-    const indicadorData = checkbox.getAttribute("data-indicador-titulo");
-    const nivel = checkbox.getAttribute("data-nivel");
-    const competencia = checkbox.getAttribute("data-competencia");
-    const competenciaData = checkbox.getAttribute("data-competencia-titulo");
-    const etapa = checkbox.getAttribute("data-etapa");
-    const etapaData = checkbox.getAttribute("data-etapa-titulo");
-    const area = checkbox.getAttribute("data-area");
-    const areaData = checkbox.getAttribute("data-area-titulo");
-
+    const indicador = checkbox.getAttribute('data-indicador');
+    const indicadorData = checkbox.getAttribute('data-indicador-titulo');
+    const nivel = checkbox.getAttribute('data-nivel');
+    const competencia = checkbox.getAttribute('data-competencia');
+    const competenciaData = checkbox.getAttribute('data-competencia-titulo');
+    const area = checkbox.getAttribute('data-area');
+    const areaData = checkbox.getAttribute('data-area-titulo');
 
     // Agregar el área, competencia y desempeño al objeto
-    if (!selectedData.hasOwnProperty(areaData)) {
+    if (!Object.prototype.hasOwnProperty.call(selectedData, areaData)) {
       selectedData[areaData] = {};
     }
-    if (!selectedData[areaData].hasOwnProperty(competenciaData)) {
+    if (
+      !Object.prototype.hasOwnProperty.call(
+        selectedData[areaData],
+        competenciaData
+      )
+    ) {
       selectedData[areaData][competenciaData] = {};
     }
 
-    if (!selectedData[areaData][competenciaData].hasOwnProperty(nivel)) {
+    if (
+      !Object.prototype.hasOwnProperty.call(
+        selectedData[areaData][competenciaData],
+        nivel
+      )
+    ) {
       selectedData[areaData][competenciaData][nivel] = [];
     }
 
     selectedData[areaData][competenciaData][nivel].push({
-      area: area,
-      areaData: areaData,
-      competencia: competencia,
-      competenciaData: competenciaData,
-      nivel: nivel,
-      indicador: indicador,
-      indicadorData: indicadorData
+      area,
+      areaData,
+      competencia,
+      competenciaData,
+      nivel,
+      indicador,
+      indicadorData,
     });
   });
   console.log(selectedData);
 
   // Limpiamos las filas de la tabla resumen (la vamos a ir rellenando ahora)
-  $("td.cell-r0").text("");
+  $('td.cell-r0').text('');
 
   // Utilizar el objeto creado para generar el texto deseado
   let texto = '';
   for (const area in selectedData) {
     texto += `<h6>Área de ${area}</h6>`;
     const competencias = selectedData[area];
-    texto += `<ul>`
+    texto += '<ul>';
     for (const competencia in competencias) {
       const niveles = competencias[competencia];
-      for (const vNivel in niveles){
+      for (const vNivel in niveles) {
         const nivel = niveles[vNivel];
 
         // siempre va haber al menos un nivel seleccionado
-        texto += `<li>`;
-        texto +=`<span style="font-weight:bold;">${nivel[0].nivel} de la competencia ${nivel[0].competencia}.</span> ${competencia} porque contribuye a trabajar `;
+        texto += '<li>';
+        texto += `<span style="font-weight:bold;">${nivel[0].nivel} de la competencia ${nivel[0].competencia}.</span> ${competencia} porque contribuye a trabajar `;
 
         // Asignamos el valor en la tabla resumen
         $(`.a${nivel[0].area}c${nivel[0].competencia[2]}`).text(vNivel);
 
         if (nivel.length == 1) {
-         texto +=`<span style="font-weight:bold;"> con el indicador ${nivel[0].indicador}.</span> ${nivel[0].indicadorData}.`;
-       } else {
-        texto +=`<span style="font-weight:bold;"> con los indicadores </span>`;
-        var i=0;
-        for (const indicador of nivel) {
-          const separador = "";
-          var ending = "";
-          if (i == nivel.length-1){
-            texto +=` y `;
-            ending = ".";
-          } else if (i > 0) {
-            texto +=`; `;
+          texto += `<span style="font-weight:bold;"> con el indicador ${nivel[0].indicador}.</span> ${nivel[0].indicadorData}.`;
+        } else {
+          texto +=
+            '<span style="font-weight:bold;"> con los indicadores </span>';
+          let i = 0;
+          for (const indicador of nivel) {
+            let ending = '';
+            if (i === nivel.length - 1) {
+              texto += ' y ';
+              ending = '.';
+            } else if (i > 0) {
+              texto += '; ';
+            }
+
+            texto += `<span style="font-weight:bold;">${indicador.indicador}.</span> ${indicador.indicadorData}${ending}`;
+            i += 1;
           }
-
-          texto +=`<span style="font-weight:bold;">${indicador.indicador}.</span> ${indicador.indicadorData}${ending}`;
-          i+=1;
-
         }
       }
+      texto += '</li>';
     }
-    texto += `</li>`;
+    texto += '</ul>';
   }
-  texto += `</ul>`
-}
 
   const modalBody = document.querySelector('.resume-text');
   modalBody.innerHTML = texto;
 
-    // concatenamos la tabla resumen (un clon)
-  const resumenTable = $("#resumen").clone();
-  $(".resume-table").html(resumenTable);
-
+  // concatenamos la tabla resumen (un clon)
+  const resumenTable = $('#resumen').clone();
+  $('.resume-table').html(resumenTable);
 }
-
 
 new ClipboardJS('.btn-primary');
 
 // Muestra el modal si estamos en un movil
-document.addEventListener("DOMContentLoaded", function() {
-  const isMobile = window.matchMedia("(max-width: 767px)").matches;
+document.addEventListener('DOMContentLoaded', function () {
+  const isMobile = window.matchMedia('(max-width: 767px)').matches;
   if (isMobile) {
-    const mobileWarningModal = new bootstrap.Modal(document.getElementById("mobileWarningModal"));
+    const mobileWarningModal = new bootstrap.Modal(
+      document.getElementById('mobileWarningModal')
+    );
     mobileWarningModal.show();
   }
 });
 
-$(document).ready(function() {
-  $('#download-button').click(function() {
+$(document).ready(function () {
+  $('#download-button').click(function () {
     html2canvas($('#resumen')[0], {
       scale: 1.5,
       width: $('.resume-table').width(),
-      height: $('.resume-table').height()
-    }).then(function(canvas) {
-      var link = document.createElement('a');
-      link.href = canvas.toDataURL("image/png");
+      height: $('.resume-table').height(),
+    }).then(function (canvas) {
+      const link = document.createElement('a');
+      link.href = canvas.toDataURL('image/png');
       link.download = 'tabla_resume.png';
       link.click();
     });
   });
 
   // Refresca la tabla cuando se teclea en buscar
-  document.getElementById('search').addEventListener('keyup', function() {
+  document.getElementById('search').addEventListener('keyup', function () {
     console.log('Search keyup event');
     fillTable(data, tableBody);
   });
 
   // Refresca la tabla cuando se cambian los checkboxes de filtro
-  var checkboxes = document.querySelectorAll('.check-filter');
-  for (var i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].addEventListener('change', function() {
+  const checkboxes = document.querySelectorAll('.check-filter');
+  for (let i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener('change', function () {
       console.log('Checkbox filter change event');
       fillTable(data, tableBody);
     });
   }
-
 });
-
